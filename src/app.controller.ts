68761d5f8c2b41b4c770723cea9b0ecd8e141c90
Takuya-Ampi <ap.takuya@gmail.com>
2022-04-21 09:42:40 +0900

    grpcサーバーをNest.jsで作成

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
+import { GrpcMethod } from '@nestjs/microservices';
+
+import { SampleData, SampleDataById } from './model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  @GrpcMethod('AppService')
+  findOne(data: SampleDataById, metadata: any): SampleData {
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ] as SampleData[];
+    const filteredItems = items.filter((item) => item.id === data.id);
+    return filteredItems.length > 0 ? filteredItems[0] : ({} as SampleData);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(5000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
+import { Transport, GrpcOptions } from '@nestjs/microservices';
+import { join } from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.createMicroservice<GrpcOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5000',
+      package: 'sample',
+      protoPath: join(process.cwd(), './proto/sample.proto'),
+    },
+  });
+  await app.listen();
 }
 bootstrap();
